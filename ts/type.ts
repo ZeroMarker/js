59298b8a817299bcf68a8072648144e1c40c4e40
Mark Chen <u78k4l67@163.com>
2023-12-14 01:20:02 +0800

    typescript

---
+++ ts/type.ts
@@ -0,0 +1,63 @@
+// 1. Static Typing
+let myNumber: number = 10;
+let myString: string = "Hello, TypeScript!";
+
+// 2. Interfaces
+interface Person {
+  name: string;
+  age: number;
+}
+
+let person: Person = { name: "John", age: 25 };
+
+// 3. Classes
+class Animal {
+  constructor(public name: string) {}
+
+  makeSound() {
+    console.log("Generic animal sound");
+  }
+}
+
+class Dog extends Animal {
+  makeSound() {
+    console.log("Bark!");
+  }
+}
+
+let myDog = new Dog("Buddy");
+myDog.makeSound();
+
+// 4. Enums
+enum Color {
+  Red,
+  Green,
+  Blue,
+}
+
+let myColor: Color = Color.Green;
+
+// 5. Generics
+function identity<T>(arg: T): T {
+  return arg;
+}
+
+let result: number = identity<number>(5);
+
+// 6. Modules
+// math.ts
+export function add(a: number, b: number): number {
+  return a + b;
+}
+
+// main.ts
+import { add } from "./math";
+let sum = add(3, 5);
+
+console.log(myNumber);
+console.log(myString);
+console.log(person);
+console.log(myDog);
+console.log(myColor);
+console.log(result);
+console.log(sum);
